49299f8a120fb013a60261f0c4908e53e8f8a959
Jim Bevington <jim.bevington@hotmail.com>
2018-03-21 14:44:12 +0000

    instantiated bookController and borrwerController in Controller

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.Book;
+
 import static spark.SparkBase.staticFileLocation;
 
 public class Controller {
@@ -7,6 +9,9 @@ public class Controller {
     public static void main(String[] args) {
 
         staticFileLocation("/public");
+
+        BookController bookController = new BookController();
+        BorrowerController borrowerController = new BorrowerController();
 
     }
 
