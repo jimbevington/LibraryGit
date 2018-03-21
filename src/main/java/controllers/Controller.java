f6392262dbd007078940da1555b6ae5181d878b6
Jim Bevington <jim.bevington@hotmail.com>
2018-03-21 14:42:51 +0000

    added staticFileLocation

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import static spark.SparkBase.staticFileLocation;
+
 public class Controller {
 
     public static void main(String[] args) {
+
+        staticFileLocation("/public");
 
     }
 
