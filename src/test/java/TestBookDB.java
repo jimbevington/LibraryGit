b474b522cfb664e84b34c9e2aaf8b932ea0cd1ad
Jim Bevington <jim.bevington@hotmail.com>
2018-03-21 16:13:46 +0000

    setup canSave test

--- src/test/java/TestBookDB.java
+++ src/test/java/TestBookDB.java
@@ -1,5 +1,25 @@
+import db.DBHelper;
+import models.Book;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestBookDB {
 
+    private Book book;
 
+    @Before
+    public void setUp() throws Exception {
+        Book book  =  new Book("All Out War", "Tim Shipman");
+        DBHelper.saveOrUpdate(book);
+    }
 
+    @Test
+    public void canSaveBook() {
+        List<Book> books = DBHelper.getAll(Book.class);
+        assertEquals(1, books.size());
+    }
 }
