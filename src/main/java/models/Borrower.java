c34fa4ccf08b0c813dcd1d26ecbfadfb51f1a833
Steven Ward <34653526+stvnwrd@users.noreply.github.com>
2018-03-21 16:21:12 +0000

    Created Borrower class

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,4 +1,49 @@
 package models;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="borrowers")
 public class Borrower {
+
+    private int id;
+    private String name;
+    private Set<Book> books;
+
+    public Borrower() {
+    }
+
+    public Borrower(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY)
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
